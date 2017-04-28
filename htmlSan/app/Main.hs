c4c023ed7e123d98c40dfff711283976525edaf2
alekaar <sternkaar@gmail.com>
2017-04-28 21:11:32 +0200

    can now sanitize attributes

--- htmlSan/app/Main.hs
+++ htmlSan/app/Main.hs
@@ -15,14 +15,28 @@ runTags html = parseTags html
 runTagTree :: String -> [TagTree String]
 runTagTree html = sanitizeTree $ parseTree html
 
---sanitize a html tree
+--sanitize a html TagTree
+--TODO maybe have to sanitize leaf
 sanitizeTree :: [TagTree String] -> [TagTree String]
-sanitizeTree []           = []
-sanitizeTree ((TagBranch s a t):htmlTree) = case elem s disallowedTags of
-  True  -> sanitizeTree htmlTree
-  False ->  [TagBranch s a (sanitizeTree t)] ++ (sanitizeTree htmlTree)
+sanitizeTree []      = []
+sanitizeTree tagtree = case tagtree of
+   [TagLeaf s] -> [TagLeaf s]
+   ((TagBranch s a t):htmlTree) -> case elem s disallowedTags of
+     True  -> sanitizeTree htmlTree
+     False -> [TagBranch s (sanitizeAttr a) (sanitizeTree t)] ++ (sanitizeTree htmlTree)
+
+--sanitize attributes to html elements
+sanitizeAttr :: [(String, String)] -> [(String, String)]
+sanitizeAttr []                = []
+sanitizeAttr ((attr, val):attrs) = case elem attr disallowedAttr of
+  True -> sanitizeAttr attrs
+  _    -> (attr, val):(sanitizeAttr attrs)
 
 
-
+--list of disallowed tags
 disallowedTags :: [String]
-disallowedTags = ["script"]
+disallowedTags = ["script", "img", "image"]
+
+--disallowed attributes to html elements
+disallowedAttr :: [String]
+disallowedAttr = ["onerror"]
