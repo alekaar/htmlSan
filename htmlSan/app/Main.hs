2bb6d738a1ea319603ee7e34ccc9c160227b64fe
alekaar <sternkaar@gmail.com>
2017-04-28 21:44:28 +0200

    can now sanitize attribute values

--- htmlSan/app/Main.hs
+++ htmlSan/app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Lib
 import Text.HTML.TagSoup
 import Text.HTML.TagSoup.Tree
+import Data.List
 
 main :: IO ()
 main = someFunc
@@ -23,7 +24,7 @@ sanitizeTree tagtree = case tagtree of
    [TagLeaf s] -> [TagLeaf s]
    ((TagBranch s a t):htmlTree) -> case elem s disallowedTags of
      True  -> sanitizeTree htmlTree
-     False -> [TagBranch s (sanitizeAttr a) (sanitizeTree t)] ++ (sanitizeTree htmlTree)
+     False -> [TagBranch s (sanitizeVals (sanitizeAttr a)) (sanitizeTree t)] ++ (sanitizeTree htmlTree)
 
 --sanitize attributes to html elements
 sanitizeAttr :: [(String, String)] -> [(String, String)]
@@ -32,6 +33,19 @@ sanitizeAttr ((attr, val):attrs) = case elem attr disallowedAttr of
   True -> sanitizeAttr attrs
   _    -> (attr, val):(sanitizeAttr attrs)
 
+--sanitize values that are assigned to attributes
+sanitizeVals :: [(String, String)] -> [(String, String)]
+sanitizeVals [] = []
+sanitizeVals ((attr, val):attrs) = case checkPrefix val disallowedVals of
+  True -> sanitizeVals attrs
+  _    -> (attr, val):(sanitizeVals attrs)
+
+--check if some disallowed value is a prefix of the supplied value
+checkPrefix :: String -> [String] -> Bool
+checkPrefix _ []         = False
+checkPrefix val (v:vals) = case isPrefixOf v val of
+  False -> checkPrefix val vals
+  _     -> True
 
 --list of disallowed tags
 disallowedTags :: [String]
@@ -39,4 +53,8 @@ disallowedTags = ["script", "img", "image"]
 
 --disallowed attributes to html elements
 disallowedAttr :: [String]
-disallowedAttr = ["onerror"]
+disallowedAttr = ["onerror", "onmouseover"]
+
+--disallowed values that can be assigned to attributes
+disallowedVals :: [String]
+disallowedVals = ["alert"]
