45a63a5501204371a2f74c0482bc6ab23a4a3730
alekaar <sternkaar@gmail.com>
2017-04-28 21:54:36 +0200

    better documentation

--- htmlSan/app/Main.hs
+++ htmlSan/app/Main.hs
@@ -12,30 +12,30 @@ main = someFunc
 runTags :: String -> [Tag String]
 runTags html = parseTags html
 
---show tagTree structure
-runTagTree :: String -> [TagTree String]
-runTagTree html = sanitizeTree $ parseTree html
+--run sanitizer on tagTree structure
+runSanitizeHTML :: String -> [TagTree String]
+runSanitizeHTML html = sanitizeTree $ parseTree html
 
 --sanitize a html TagTree
---TODO maybe have to sanitize leaf
+--TODO maybe have to sanitize leaf and check for &lt; and &gt;
 sanitizeTree :: [TagTree String] -> [TagTree String]
 sanitizeTree []      = []
 sanitizeTree tagtree = case tagtree of
-   [TagLeaf s] -> [TagLeaf s]
+   [TagLeaf s]                  -> [TagLeaf s]
    ((TagBranch s a t):htmlTree) -> case elem s disallowedTags of
      True  -> sanitizeTree htmlTree
      False -> [TagBranch s (sanitizeVals (sanitizeAttr a)) (sanitizeTree t)] ++ (sanitizeTree htmlTree)
 
 --sanitize attributes to html elements
 sanitizeAttr :: [(String, String)] -> [(String, String)]
-sanitizeAttr []                = []
+sanitizeAttr []                  = []
 sanitizeAttr ((attr, val):attrs) = case elem attr disallowedAttr of
   True -> sanitizeAttr attrs
   _    -> (attr, val):(sanitizeAttr attrs)
 
 --sanitize values that are assigned to attributes
 sanitizeVals :: [(String, String)] -> [(String, String)]
-sanitizeVals [] = []
+sanitizeVals []                  = []
 sanitizeVals ((attr, val):attrs) = case checkPrefix val disallowedVals of
   True -> sanitizeVals attrs
   _    -> (attr, val):(sanitizeVals attrs)
@@ -48,13 +48,24 @@ checkPrefix val (v:vals) = case isPrefixOf v val of
   _     -> True
 
 --list of disallowed tags
+{-
+script: The most obvious tag to block, can be used to run scripts directly.
+img/image: A faulty image source could potentially be dangerous.
+-}
 disallowedTags :: [String]
 disallowedTags = ["script", "img", "image"]
 
 --disallowed attributes to html elements
+{-
+onerror: Can be used in combination with a faulty img to run scripts.
+onmouseover: Can run scripts if clients mouse hover over element.
+-}
 disallowedAttr :: [String]
 disallowedAttr = ["onerror", "onmouseover"]
 
---disallowed values that can be assigned to attributes
+--disallowed prefixes of values that can be assigned to attributes
+{-
+alert: Makes a popup.
+-}
 disallowedVals :: [String]
 disallowedVals = ["alert"]
